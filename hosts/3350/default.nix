ec7baf0d4f5188b349d1523e39f30277328c42bd
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-02 02:43:15 -0500

    hosts/3350: remove `intel-media-driver`

--- hosts/3350/default.nix
+++ hosts/3350/default.nix
@@ -47,7 +47,8 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      # holy hecc the older driver is actually better
+      #intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
