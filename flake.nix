abbc8198f5def0e6b0c39df59222056dfc9a3cdc
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-09 21:51:38 -0500

    flake.nix: improve readability (hopefully:tm:)

--- flake.nix
+++ flake.nix
@@ -31,6 +31,97 @@
         config = { allowUnfree = true; };
       };
       lib = nixpkgs.lib;
+      # configuration to enable stuff from `nixflake-misc`
+      miscConfig =
+        # i'll comment this properly later, am feeling lazy
+        ({
+          environment.systemPackages = with pkgs; [
+            # install nix-software-center
+            nix-software-center.packages.${system}.nix-software-center
+            nixflake-misc.packages.${system}.egmde
+            nixpkgs-unstable.legacyPackages.${system}.miriway
+            nixpkgs-unstable.legacyPackages.${system}.mir
+            nixflake-misc.packages.${system}.sfwbar
+            ## these are required for my miriwway-based rice
+            # the example mir shell enabled in the sessions below depends on gnome-terminal
+            gnome.gnome-terminal
+            # required for setting background in miriway
+            swaybg
+            # top bar for wayland
+            waybar
+            # notification thingy
+            mako
+            # screenshot utilities
+            wf-recorder
+            slurp
+            grim
+            # admin prompt
+            lxqt.lxqt-policykit
+          ];
+          # enable qvwm and some wayland compositors
+          services.xserver.displayManager.sessionPackages = [
+            nixflake-misc.packages.${system}.qvwm
+            nixpkgs-unstable.legacyPackages.${system}.miriway
+            nixpkgs-unstable.legacyPackages.${system}.mir
+          ];
+          # also required for mir-based sessions
+          xdg.portal.extraPortals = with pkgs; [
+            xdg-desktop-portal-wlr
+            #xdg-desktop-portal-gtk
+          ];
+        });
+      # defined here bc im a lazy goober who still cant write proper code
+      lomiriConfig =
+        # stinky theft (ty OPNA TUTUTUTUTUT)
+        (
+          let
+            baseconfig = { allowUnfree = true; };
+            unstable = import nixpkgs-lomiri { inherit system; config = baseconfig; };
+          in
+          {
+            imports = [ (nixpkgs-lomiri + "/nixos/modules/programs/lomiri.nix") ];
+
+            disabledModules = [ "programs/lomiri.nix" ];
+
+            programs.lomiri.enable = true;
+
+            nixpkgs.config = baseconfig // {
+              packageOverrides = pkgs: {
+                lomiri-session = unstable.lomiri-session;
+                hfd-service = unstable.hfd-service;
+                repowerd = unstable.repowerd;
+                lomiri = unstable.lomiri;
+                qtmir = unstable.qtmir;
+                libayatana-common = unstable.libayatana-common;
+                lomiri-thumbnailer = unstable.lomiri-thumbnailer;
+                lomiri-url-dispatcher = unstable.lomiri-url-dispatcher;
+                lomiri-click = unstable.lomiri-click;
+                lomiri-schemas = unstable.lomiri-schemas;
+                history-service = unstable.history-service;
+                telephony-service = unstable.telephony-service;
+                telepathy-mission-control = unstable.telepathy-mission-control;
+                ubuntu-themes = unstable.ubuntu-themes;
+                vanilla-dmz = unstable.vanilla-dmz;
+                ayatana-indicator-application = unstable.ayatana-indicator-application;
+                ayatana-indicator-bluetooth = unstable.ayatana-indicator-bluetooth;
+                ayatana-indicator-datetime = unstable.ayatana-indicator-datetime;
+                ayatana-indicator-display = unstable.ayatana-indicator-display;
+                ayatana-indicator-keyboard = unstable.ayatana-indicator-keyboard;
+                ayatana-indicator-messages = unstable.ayatana-indicator-messages;
+                ayatana-indicator-notifications = unstable.ayatana-indicator-notifications;
+                ayatana-indicator-power = unstable.ayatana-indicator-power;
+                ayatana-indicator-printers = unstable.ayatana-indicator-printers;
+                ayatana-indicator-session = unstable.ayatana-indicator-session;
+                ayatana-indicator-sound = unstable.ayatana-indicator-sound;
+                lomiri-indicator-network = unstable.lomiri-indicator-network;
+                ubports-click = unstable.ubports-click;
+                lomiri-download-manager = unstable.lomiri-download-manager;
+                lomiri-system-settings = unstable.lomiri-system-settings;
+                content-hub = unstable.content-hub;
+              };
+            };
+          }
+        );
     in
     {
       # my main devices are defined here
@@ -60,93 +151,8 @@
             ./package-sets/lsp.nix
             ./package-sets/media.nix
             ./package-sets/themes.nix
-            # aaaaaaaaaa
-            ({ config, pkgs, ... }: {
-              environment.systemPackages = with pkgs; [
-                # install nix-software-center
-                nix-software-center.packages.${system}.nix-software-center
-                nixflake-misc.packages.${system}.egmde
-                nixpkgs-unstable.legacyPackages.${system}.miriway
-                nixpkgs-unstable.legacyPackages.${system}.mir
-                nixflake-misc.packages.${system}.sfwbar
-                ## these are required for my miriwway-based rice
-                # the example mir shell enabled in the sessions below depends on gnome-terminal
-                gnome.gnome-terminal
-                # required for setting background in miriway
-                swaybg
-                # top bar for wayland
-                waybar
-                # notification thingy
-                mako
-                # screenshot utilities
-                wf-recorder
-                slurp
-                grim
-                # admin prompt
-                lxqt.lxqt-policykit
-              ];
-              # enable qvwm and some wayland compositors
-              services.xserver.displayManager.sessionPackages = [
-                nixflake-misc.packages.${system}.qvwm
-                nixpkgs-unstable.legacyPackages.${system}.miriway
-                nixpkgs-unstable.legacyPackages.${system}.mir
-              ];
-              # also required for mir-based sessions
-              xdg.portal.extraPortals = with pkgs; [
-                xdg-desktop-portal-wlr
-                #xdg-desktop-portal-gtk
-              ];
-            })
-            # lomiri
-            (
-              let
-                baseconfig = { allowUnfree = true; };
-                unstable = import nixpkgs-lomiri { inherit system; config = baseconfig; };
-              in
-              {
-                imports = [ (nixpkgs-lomiri + "/nixos/modules/programs/lomiri.nix") ];
-
-                disabledModules = [ "programs/lomiri.nix" ];
-
-                programs.lomiri.enable = true;
-
-                nixpkgs.config = baseconfig // {
-                  packageOverrides = pkgs: {
-                    lomiri-session = unstable.lomiri-session;
-                    hfd-service = unstable.hfd-service;
-                    repowerd = unstable.repowerd;
-                    lomiri = unstable.lomiri;
-                    qtmir = unstable.qtmir;
-                    libayatana-common = unstable.libayatana-common;
-                    lomiri-thumbnailer = unstable.lomiri-thumbnailer;
-                    lomiri-url-dispatcher = unstable.lomiri-url-dispatcher;
-                    lomiri-click = unstable.lomiri-click;
-                    lomiri-schemas = unstable.lomiri-schemas;
-                    history-service = unstable.history-service;
-                    telephony-service = unstable.telephony-service;
-                    telepathy-mission-control = unstable.telepathy-mission-control;
-                    ubuntu-themes = unstable.ubuntu-themes;
-                    vanilla-dmz = unstable.vanilla-dmz;
-                    ayatana-indicator-application = unstable.ayatana-indicator-application;
-                    ayatana-indicator-bluetooth = unstable.ayatana-indicator-bluetooth;
-                    ayatana-indicator-datetime = unstable.ayatana-indicator-datetime;
-                    ayatana-indicator-display = unstable.ayatana-indicator-display;
-                    ayatana-indicator-keyboard = unstable.ayatana-indicator-keyboard;
-                    ayatana-indicator-messages = unstable.ayatana-indicator-messages;
-                    ayatana-indicator-notifications = unstable.ayatana-indicator-notifications;
-                    ayatana-indicator-power = unstable.ayatana-indicator-power;
-                    ayatana-indicator-printers = unstable.ayatana-indicator-printers;
-                    ayatana-indicator-session = unstable.ayatana-indicator-session;
-                    ayatana-indicator-sound = unstable.ayatana-indicator-sound;
-                    lomiri-indicator-network = unstable.lomiri-indicator-network;
-                    ubports-click = unstable.ubports-click;
-                    lomiri-download-manager = unstable.lomiri-download-manager;
-                    lomiri-system-settings = unstable.lomiri-system-settings;
-                    content-hub = unstable.content-hub;
-                  };
-                };
-              }
-            )
+            miscConfig
+            lomiriConfig
           ];
         };
         Gem-3350 = lib.nixosSystem {
@@ -173,93 +179,8 @@
             ./package-sets/gayming.nix
             ./package-sets/media.nix
             ./package-sets/themes.nix
-            # i'll comment this properly later, am feeling lazy
-            ({ config, pkgs, ... }: {
-              environment.systemPackages = with pkgs; [
-                # install nix-software-center
-                nix-software-center.packages.${system}.nix-software-center
-                nixflake-misc.packages.${system}.egmde
-                nixpkgs-unstable.legacyPackages.${system}.miriway
-                nixpkgs-unstable.legacyPackages.${system}.mir
-                nixflake-misc.packages.${system}.sfwbar
-                ## these are required for my miriwway-based rice
-                # the example mir shell enabled in the sessions below depends on gnome-terminal
-                gnome.gnome-terminal
-                # required for setting background in miriway
-                swaybg
-                # top bar for wayland
-                waybar
-                # notification thingy
-                mako
-                # screenshot utilities
-                wf-recorder
-                slurp
-                grim
-                # admin prompt
-                lxqt.lxqt-policykit
-              ];
-              # enable qvwm and some wayland compositors
-              services.xserver.displayManager.sessionPackages = [
-                nixflake-misc.packages.${system}.qvwm
-                nixpkgs-unstable.legacyPackages.${system}.miriway
-                nixpkgs-unstable.legacyPackages.${system}.mir
-              ];
-              # also required for mir-based sessions
-              xdg.portal.extraPortals = with pkgs; [
-                xdg-desktop-portal-wlr
-                #xdg-desktop-portal-gtk
-              ];
-            })
-            # stinky theft
-            (
-              let
-                baseconfig = { allowUnfree = true; };
-                unstable = import nixpkgs-lomiri { inherit system; config = baseconfig; };
-              in
-              {
-                imports = [ (nixpkgs-lomiri + "/nixos/modules/programs/lomiri.nix") ];
-
-                disabledModules = [ "programs/lomiri.nix" ];
-
-                programs.lomiri.enable = true;
-
-                nixpkgs.config = baseconfig // {
-                  packageOverrides = pkgs: {
-                    lomiri-session = unstable.lomiri-session;
-                    hfd-service = unstable.hfd-service;
-                    repowerd = unstable.repowerd;
-                    lomiri = unstable.lomiri;
-                    qtmir = unstable.qtmir;
-                    libayatana-common = unstable.libayatana-common;
-                    lomiri-thumbnailer = unstable.lomiri-thumbnailer;
-                    lomiri-url-dispatcher = unstable.lomiri-url-dispatcher;
-                    lomiri-click = unstable.lomiri-click;
-                    lomiri-schemas = unstable.lomiri-schemas;
-                    history-service = unstable.history-service;
-                    telephony-service = unstable.telephony-service;
-                    telepathy-mission-control = unstable.telepathy-mission-control;
-                    ubuntu-themes = unstable.ubuntu-themes;
-                    vanilla-dmz = unstable.vanilla-dmz;
-                    ayatana-indicator-application = unstable.ayatana-indicator-application;
-                    ayatana-indicator-bluetooth = unstable.ayatana-indicator-bluetooth;
-                    ayatana-indicator-datetime = unstable.ayatana-indicator-datetime;
-                    ayatana-indicator-display = unstable.ayatana-indicator-display;
-                    ayatana-indicator-keyboard = unstable.ayatana-indicator-keyboard;
-                    ayatana-indicator-messages = unstable.ayatana-indicator-messages;
-                    ayatana-indicator-notifications = unstable.ayatana-indicator-notifications;
-                    ayatana-indicator-power = unstable.ayatana-indicator-power;
-                    ayatana-indicator-printers = unstable.ayatana-indicator-printers;
-                    ayatana-indicator-session = unstable.ayatana-indicator-session;
-                    ayatana-indicator-sound = unstable.ayatana-indicator-sound;
-                    lomiri-indicator-network = unstable.lomiri-indicator-network;
-                    ubports-click = unstable.ubports-click;
-                    lomiri-download-manager = unstable.lomiri-download-manager;
-                    lomiri-system-settings = unstable.lomiri-system-settings;
-                    content-hub = unstable.content-hub;
-                  };
-                };
-              }
-            )
+            miscConfig
+            lomiriConfig
           ];
         };
         Gem-Super = lib.nixosSystem {
