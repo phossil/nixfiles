f362a75a8ebd03d4863f5e2b990621a67107cf35
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-11 17:42:47 -0500

    hosts/aswitch: add opengl drivers

--- hosts/aswitch/default.nix
+++ hosts/aswitch/default.nix
@@ -14,6 +14,25 @@
       ./hardware-configuration.nix
     ];
 
+  # intel graphics
+  nixpkgs.config.packageOverrides = pkgs: {
+    # i want to play youtube videos without h.264, ty
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+      # opencl on a laptop ???
+      intel-compute-runtime
+    ];
+    # enable 32-bit graphics support because Steam 
+    driSupport32Bit = true;
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
