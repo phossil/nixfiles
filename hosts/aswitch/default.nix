81fccfc09a49093f69560b2c4b9faea7aa3c4618
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-11 01:17:15 -0500

    hosts/aswitch: add `waypipe` and `netsurf-browser`

--- hosts/aswitch/default.nix
+++ hosts/aswitch/default.nix
@@ -69,6 +69,10 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+
+    # required for configuring syncthing "locally"
+    waypipe
+    netsurf-browser
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
