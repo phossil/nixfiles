11f99b833498d44638d8921092a459ef9712af53
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-05-15 00:28:53 -0400

    common/plymouth: replace logo with cute yumoji c:

--- common/plymouth.nix
+++ common/plymouth.nix
@@ -11,8 +11,14 @@
       # as azu [2] for ripping the sprite from a fighting game known as
       # mystic chain [3], so if anyone knows of a place where some of
       # the assets are online please let me know
-      url = "https://ninetales.neocities.org/img/yuyuko_attack_by_generalcirno-d36n1jf.gif";
-      sha256 = "t4IY20W0O5+qZ3/mSZcFpq4UeIquXmQXBQYX9MHeFjc=";
+      #url = "https://ninetales.neocities.org/img/yuyuko_attack_by_generalcirno-d36n1jf.gif";
+      #sha256 = "t4IY20W0O5+qZ3/mSZcFpq4UeIquXmQXBQYX9MHeFjc=";
+
+      # plymouth wants png's x.x
+      # we're using these cute emojis by duck instead :3
+      # https://twitter.com/i/web/status/1054064623274487808
+      url = "https://pbs.twimg.com/media/DqDJ5L-U4AAFjtQ.png";
+      sha256 = "18xdl5fyzjmmi6x0yr8lmgwq0hnxjzcswafi8i6rx5q95rx43snk";
     };
   };
 }
