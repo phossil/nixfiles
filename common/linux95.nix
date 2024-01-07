9ea8fa6a4029c38896471d28ab1b637beffa52f0
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2024-01-07 17:12:55 -0500

    package-sets/kernels: move file

--- common/linux95.nix
+++ common/linux95.nix
@@ -5,17 +5,6 @@
   nixpkgs = {
     overlays = [
       (final: prev: {
-        # make a custom config for the latest kernel
-        linux_latest_98se = pkgs.linuxPackagesFor (pkgs.linux_latest.override {
-          structuredExtraConfig = with lib.kernel; {
-            # add 16-bit support bc deerhunter 3
-            # pls check the linux kernel driver db for x86_16bit
-            EXPERT = yes;
-            MODIFY_LDT_SYSCALL = yes;
-            X86_16BIT = yes;
-          };
-          ignoreConfigErrors = true;
-        });
         # temporary override until 6.7 is officially released
         linuxPackages_testing = pkgs.linuxPackagesFor
           (pkgs.linux_testing.override {
