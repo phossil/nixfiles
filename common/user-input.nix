1ea893bb76e3265e0022fdf1d423e7e4d46ccb0b
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2024-03-02 00:04:20 -0500

    common/user-input: `xkbOptions`

--- common/user-input.nix
+++ common/user-input.nix
@@ -9,9 +9,7 @@
       layout = "us";
       # `ctrl:nocaps`: Map Caps Lock as an additional Ctrl key
       # (the vanilla Caps Lock key is useless to me)
-      # `compose:menu`: Map the Menu key as the Compose key
-      # (the Compose key is useful for inputing "exotic" characters)
-      xkbOptions = "ctrl:nocaps,compose:menu";
+      xkbOptions = "ctrl:nocaps";
       # Enable touchpad with tap-to-click support
       libinput.touchpad.tapping = true;
     };
