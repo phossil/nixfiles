3ecf0ba27788830540d7cbdabecd13c7d7452ba6
pennimew pettals <38191182+phossil@users.noreply.github.com>
2024-06-01 06:53:44 -0400

    common/shell: add atuin

--- common/shell.nix
+++ common/shell.nix
@@ -2,13 +2,18 @@
 
 {
   # install zsh with autosuggestions
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    # history file size
-    histSize = 100000;
-  };
+  programs.zsh.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+
   # this is absolutely mandatory, i swear
   programs.starship.enable = true;
   programs.starship.settings = import ./starship.toml.nix;
+
+  # i want my time stamps
+  services.atuin.enable = true;
+  # 128 * 1024 makes sense, right ?
+  services.atuin.maxHistoryLength = 131072;
+  # the above only enables the server, a client instance is needed
+  # TODO: add to global zshrc
+  environment.systemPackages = [ pkgs.atuin ];
 }
